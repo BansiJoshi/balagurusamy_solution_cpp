d0270186c6e139960ac4238c50aa84338ff1861a
BANSI JOSHI <bkjoshi14297@gmail.com>
2017-12-15 18:48:45 +0530

    update ex

--- Exercise_7/ex_7.1_overload_arithmetic_operators.cpp
+++ Exercise_7/ex_7.1_overload_arithmetic_operators.cpp
@@ -8,71 +8,71 @@ using namespace std;
  
 class FLOAT
 { 
-           float data;
-           public:
-           FLOAT(){};
-           FLOAT(float d)
-           { data=d;}
-           FLOAT operator+(FLOAT f1);
-           FLOAT operator-(FLOAT f2);
-           FLOAT operator*(FLOAT f3);
-           FLOAT operator/(FLOAT f4);
-           void display();
+   float data;
+   public:
+   FLOAT(){};
+   FLOAT(float d)
+   { data=d;}
+   FLOAT operator+(FLOAT f1);
+   FLOAT operator-(FLOAT f2);
+   FLOAT operator*(FLOAT f3);
+   FLOAT operator/(FLOAT f4);
+   void display();
 };
 
 FLOAT FLOAT::operator+(FLOAT f1)
 {
-           FLOAT temp;
-           temp.data=data+f1.data;
-           return (temp);
+   FLOAT temp;
+   temp.data=data+f1.data;
+   return (temp);
 }
 
 FLOAT FLOAT::operator-(FLOAT f2)
 {
-          FLOAT temp;
-          temp.data=data-f2.data;
-          return temp;
+   FLOAT temp;
+   temp.data=data-f2.data;
+   return temp;
 }
 
 FLOAT FLOAT::operator*(FLOAT f3)
 {
-          FLOAT temp;
-          temp.data=data*f3.data;
-          return temp;
+   FLOAT temp;
+   temp.data=data*f3.data;
+   return temp;
 }
 
 FLOAT FLOAT::operator/(FLOAT f4)
 {
-             FLOAT temp;
-             temp.data=data/f4.data;
-             return (temp);
+   FLOAT temp;
+   temp.data=data/f4.data;
+   return (temp);
 }
 
 void FLOAT:: display()
 {
-             cout<<data<<"\n";
+   cout<<data<<"\n";
 }
 
 int main()
 {
-                      FLOAT F1,F2,F3,F4,F5,F6;
-                      F1=FLOAT(2.5);
-                      F2=FLOAT(1.5);
-                      F3=F1+F2;
-                      F4=F1-F2;
-                      F5=F1*F2;
-                      F6=F1/F2;
-                      cout<<" F1 = ";
-                      F1.display();
-                      cout<<" F2 = ";
-                      F2.display();
-                      cout<<" F1+F2 = ";
-                      F3.display();
-                      cout<<" F1-F2 = ";
-                      F4.display();
-                      cout<<" F1*F2 = ";
-                      F5.display();
-                      cout<<" F1/F2= ";
-                      F6.display();
-     return 0;
+   FLOAT F1,F2,F3,F4,F5,F6;
+   F1=FLOAT(2.5);
+   F2=FLOAT(1.5);
+   F3=F1+F2;
+   F4=F1-F2;
+   F5=F1*F2;
+   F6=F1/F2;
+   cout<<" F1 = ";
+   F1.display();
+   cout<<" F2 = ";
+   F2.display();
+   cout<<" F1+F2 = ";
+   F3.display();
+   cout<<" F1-F2 = ";
+   F4.display();
+   cout<<" F1*F2 = ";
+   F5.display();
+   cout<<" F1/F2= ";
+   F6.display();
+   return 0;
 }
